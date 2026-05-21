b376c1426a37637a280fd051654d304b686197f7
NehuenZ12 <aldonehuen123@gmail.com>
2026-05-21 17:34:24 -0300

    Estructura
    clases

---
+++ src/enums/EstadoCivil.java
@@ -0,0 +1,9 @@
+package enums;
+
+/**
+ *
+ * @author Nehuen
+ */
+public enum EstadoCivil {
+    
+}
---
+++ src/grupo10_medianaranja/main.java
@@ -0,0 +1,16 @@
+package grupo10_medianaranja;
+
+/**
+ *
+ * @author matia, Nehuen Zerdá
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
